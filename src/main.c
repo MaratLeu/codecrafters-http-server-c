1823a17b5b2c4c2e095af843b460e107c081777c
maratlev <leon24082006@gmail.com>
2025-07-08 01:25:27 +0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -46,10 +46,13 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	//
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	int client = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
 	//
-	close(server_fd);
+    char* response = "HTTP/1.1 200 OK\r\n\r\n";
+    send(client, response, strlen(response), 0);
+    
+    close(server_fd);
 
 	return 0;
 }
